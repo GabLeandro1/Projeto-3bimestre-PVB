7792c49fec59d95a0a3dbe75e7a4330a9d9e7e34
gabri <gabrielpaiva00089@gmail.com>
2026-02-11 19:46:57 -0300

    first commit

---
+++ Projeto_Wagner_3bi/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Projeto_Wagner_3bi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o valor do produto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite a quantidade de parcelas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(133, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Digite a data da compra";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(312, 60);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(25, 137);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(371, 60);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(73, 137);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(626, 117);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Digite a data da parcela a ser paga";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(665, 66);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(486, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Digite a parcela a ser paga";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 35);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Calcular parcelas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(570, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 35);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Pagamentos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(337, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Valor por parcela";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(337, 261);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "0";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Projeto_Wagner_3bi/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Wagner_3bi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        double ValorReajustado;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            textBox5.Clear(); 
+            float valorAPagar = float.Parse(textBox1.Text);
+            int qtdParcelas = int.Parse(textBox2.Text);
+            float parcelaParaPagamento = valorAPagar / qtdParcelas;
+            double valorPorParcela = Math.Round(parcelaParaPagamento, 2);
+            ValorReajustado = valorPorParcela;
+            DateTime dataTeste;
+            string dataPagemento = textBox3.Text;
+            Boolean testarData = DateTime.TryParse(dataPagemento, out dataTeste);
+            if (testarData == false)
+                MessageBox.Show("A data não foi escrita corretamente");
+            else
+            {
+                DateTime dataPorParcela = dataTeste;
+                for (int i = 1; i <= qtdParcelas; i++)
+                {
+                    DateTime dataParaImprimir;
+                    dataParaImprimir = dataPorParcela.AddDays(31);
+                    dataPorParcela = dataParaImprimir;
+                    int day = (int)dataPorParcela.DayOfWeek;
+                    if (day == 0)
+                    {
+                        dataPorParcela = dataPorParcela.AddDays(1);
+                    }
+                    else if (day == 6)
+                    {
+                        dataPorParcela = dataPorParcela.AddDays(2);
+                    }
+                    textBox4.AppendText(i.ToString("0") + Environment.NewLine);
+                    textBox5.AppendText(dataPorParcela.ToString("dd/MM/yyyy") + Environment.NewLine);
+                    label7.Text = String.Format("{0:C2}", valorPorParcela);
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int totalParcelas = int.Parse(textBox2.Text);
+            int parcelaParaPagamento = int.Parse(textBox6.Text);
+            string dataParaPagamento = textBox7.Text;
+            DateTime data;
+            Boolean testarData = DateTime.TryParse(dataParaPagamento, out data);
+            if (testarData == false)
+                MessageBox.Show("A data não foi escrita corretamente");
+            else
+            {
+                for (int i = 0; i <= textBox5.Lines.Length - 1; i++)
+                {
+                    DateTime dataParaTeste;
+                    string datas = textBox5.Lines[i];
+                    Boolean testarData2 = DateTime.TryParse(datas, out dataParaTeste);
+                    if (testarData2 == true)
+                    {
+                        int parcelaNaTextBox = int.Parse(textBox4.Lines[i]);
+                        if (parcelaParaPagamento == parcelaNaTextBox)
+                        {
+                            if (data <= dataParaTeste)
+                            {
+                                textBox4.Clear();
+                                textBox5.Clear();
+                                MessageBox.Show("Pagamento feito com sucesso");
+                                for (int y = parcelaParaPagamento + 1; y <= totalParcelas; y++)
+                                {
+                                    dataParaTeste = dataParaTeste.AddDays(31);
+                                    int dia = (int)dataParaTeste.DayOfWeek;
+                                    if (dia == 0)
+                                    {
+                                        dataParaTeste = dataParaTeste.AddDays(1);
+                                    }
+                                    else if (dia == 6)
+                                    {
+                                        dataParaTeste = dataParaTeste.AddDays(2);
+                                    }
+                                    textBox4.AppendText(y.ToString("0") + Environment.NewLine);
+                                    textBox5.AppendText(dataParaTeste.ToString("dd/MM/yyyy") + Environment.NewLine);                                 
+                                }
+                                break;
+                            } else if (data > dataParaTeste)
+                            {
+                                ValorReajustado = ValorReajustado + (ValorReajustado * 0.03);
+                                label7.Text = String.Format("{0:C2}", ValorReajustado);
+                                textBox4.Clear();
+                                textBox5.Clear();
+                                MessageBox.Show("Parcela em atraso!");
+                                for (int y = parcelaParaPagamento + 1; y <= totalParcelas; y++)
+                                {
+                                    dataParaTeste = dataParaTeste.AddDays(31);
+                                    int dia = (int)dataParaTeste.DayOfWeek;
+                                    if (dia == 0)
+                                    {
+                                        dataParaTeste = dataParaTeste.AddDays(1);
+                                    }
+                                    else if (dia == 6)
+                                    {
+                                        dataParaTeste = dataParaTeste.AddDays(2);
+                                    }
+                                    textBox4.AppendText(y.ToString("0") + Environment.NewLine);
+                                    textBox5.AppendText(dataParaTeste.ToString("dd/MM/yyyy") + Environment.NewLine);
+                                }
+                                break;
+                            }
+                        } else if (parcelaParaPagamento > parcelaNaTextBox || parcelaParaPagamento < parcelaNaTextBox)
+                        {
+                            MessageBox.Show("Não pode pagar parcela fora da sua data de vencimento");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
